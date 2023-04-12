d2e1455a47e107000887d122a133b379272f9770
zinaida <zinaida.j84@gmail.com>
2023-04-12 13:30:23 +0200

    test Areas Added

--- src/main/java/com/codecool/mightytextadventure/Application.java
+++ src/main/java/com/codecool/mightytextadventure/Application.java
@@ -22,13 +22,13 @@ public class Application {
 
     private static Area[] loadAreas(){
         Area[] areas =   new Area[7];
-        areas[0] = new Area("Start room");
-        areas[1] = new Area("Room 1");
-        areas[2] = new Area("Room 2");
-        areas[3] = new Area("Room 3");
-        areas[4] = new Area("Room 4");
+        areas[0] = new Area("Start room", new int[]{1, 2}, new String[]{"Yes(1)", "No(2)"});
+        areas[1] = new Area("Go to End", new int[]{6, 3}, new String[]{"Yes(1)", "No(2)"});
+        areas[2] = new Area("You have to go to End", new int[]{6},new String[]{"Yes"});
+        areas[3] = new Area("Room 3", new int[]{1, 2, 4}, new String[]{"Yes(1)", "No(2)", "I don't know(3)"});
+        areas[4] = new Area("You're trapped");
         areas[5] = new Area("Room 5");
-        areas[6] = new Area("Room 6");
+        areas[6] = new Area("End");
         return areas;
     }
 }
