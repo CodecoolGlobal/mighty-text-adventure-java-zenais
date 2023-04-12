18c3fbc8327407e137a290f317ab0559999bacc9
Thomas S <thomas.scy@gmail.com>
2023-04-12 20:08:53 +0200

    added method printScannerQuestion

--- src/main/java/com/codecool/mightytextadventure/ui/Display.java
+++ src/main/java/com/codecool/mightytextadventure/ui/Display.java
@@ -5,5 +5,8 @@ public class Display {
     public void printMessage(String message) {
         System.out.println(message);
     }
+    public void printScannerQuestion(String message) {
+        System.out.print(message);
+    }
 
 }
