0135ec777f9ec9a9aa6fb5b68f587018e9307cfb
Thomas S <thomas.scy@gmail.com>
2023-04-12 10:56:56 +0200

    added class player

---
+++ src/main/java/com/codecool/mightytextadventure/data/Player.java
@@ -0,0 +1,40 @@
+package com.codecool.mightytextadventure.data;
+
+import java.util.ArrayList;
+
+public class Player {
+    private String name;
+    private Area area;
+    private ArrayList<String> items;
+
+
+    public Player(String name, Area area, ArrayList<String> items) {
+        this.name = name;
+        this.area = area;
+        this.items = items;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Area getArea() {
+        return area;
+    }
+
+    public ArrayList<String> getItems() {
+        return items;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Area setArea() {
+        return area;
+    }
+
+    public ArrayList<String> setItems() {
+        return items;
+    }
+}
