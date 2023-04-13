2140711cad13f385295d03875b9f5fff85b118d0
Thomas S <thomas.scy@gmail.com>
2023-04-13 11:34:43 +0200

    added input handler for "help"

--- src/main/java/com/codecool/mightytextadventure/ui/Input.java
+++ src/main/java/com/codecool/mightytextadventure/ui/Input.java
@@ -4,17 +4,25 @@ import java.util.Scanner;
 
 public class Input {
 
-    public Integer getInputFromUser(){
+    public Integer getInputFromUser() {
         Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.next();
         Integer choice = -1;
         try {
-            choice = Integer.parseInt(scanner.next());
-        }catch (NumberFormatException e){
+            if (userInput.equalsIgnoreCase("help") ||
+                    userInput.equalsIgnoreCase("h")) {
+                Display.printHelp();
+                return -2;
+            }
+            choice = Integer.parseInt(userInput);
+
+        } catch (NumberFormatException e) {
             System.out.println("Did you fall asleep?");
         }
         return choice;
     }
-    public String getInputStringFromUser(){
+
+    public String getInputStringFromUser() {
         Scanner scanner = new Scanner(System.in);
         return scanner.next();
     }
