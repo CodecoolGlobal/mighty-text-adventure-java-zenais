f92c88243ef77e049576cd87169515151a4e462f
zinaida <zinaida.j84@gmail.com>
2023-04-13 17:52:27 +0200

    magic numbers added

--- src/main/java/com/codecool/mightytextadventure/ui/Input.java
+++ src/main/java/com/codecool/mightytextadventure/ui/Input.java
@@ -3,15 +3,17 @@ package com.codecool.mightytextadventure.ui;
 import java.util.Scanner;
 
 public class Input {
+    public static final int INVALID_CHOICE = -1;
+    public static final int HELP = -2;
     Scanner scanner = new Scanner(System.in);
 
     public Integer getInputFromUser() {
         String userInput = scanner.next();
-        Integer choice = -1;
+        Integer choice = INVALID_CHOICE;
         try {
             if (userInput.equalsIgnoreCase("help") ||
                     userInput.equalsIgnoreCase("h")) {
-                return -2;
+                return HELP;
             }
             choice = Integer.parseInt(userInput);
 
